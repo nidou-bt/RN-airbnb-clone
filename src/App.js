0b63b7d7d70c5247a95d1181f3f2f100ecdc31e5
nidhal-bt <bentaaritn@gmail.com>
2023-06-04 22:55:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 40,
+  },
+});
